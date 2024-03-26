27961812a94957098e20a4b83936a5de33bd514a
Martins Ndifon <martinsndifon@gmail.com>
2024-03-26 15:10:13 +0100

    Create Iphone 3D Model

--- app/page.js
+++ app/page.js
@@ -2,11 +2,15 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 
 export default function Home() {
   return (
-    <main className="bg-black">
-      <Navbar /> <Hero /> <Highlights />
+    <main className="bg-black" id="root">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
     </main>
   );
 }
